1054fbcce8100207453c33dec007ff4880a7fbef
smartapant <honeypie94@gmail.com>
2013-03-12 16:48:47 +0300

    now its working, but only if player.points<=21

--- lib/game.rb
+++ lib/game.rb
@@ -7,17 +7,14 @@ class Game
   def winner
     @winners = []
     number=21
-    until @winners != [] and number > 0
+    while @winners == [] and number > 0
       if @players.each {|player| player.points<=21}
       @players.each do |player|
         @winners<<player.name if player.points == number
-        number-=1
-      end
-      else
-        @players.each do |player|
-          @winners<<player.name if player.points == number
-          number+=1
         end
+      number-=1
+      elsif players.each {|player| player.points>number}
+
       end
     end
    # @winners.index(0) {|id,winner| @winners.delete_at(id); @winners<<"#{winner.name}, #{winner.points}"}
--- spec/game.rb
+++ spec/game.rb
@@ -6,8 +6,8 @@ require '../lib/game'
 puts players=[Player.new('p1'),Player.new('p2')]
 puts game=Game.new(players)
 
-puts players[0].add_points(25)
-puts players[1].add_points(26)
+puts players[0].add_points(20)
+puts players[1].add_points(19)
 
 puts game.winner
 puts game.winners
